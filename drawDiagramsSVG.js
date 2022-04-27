0d65297414b4f6fafa4c9a752a647bc5f7f16159
fersoler <fsoler@us.es>
2022-04-27 11:22:03 +0200

    Added pentagon diagrams

--- drawDiagramsSVG.js
+++ drawDiagramsSVG.js
@@ -43,6 +43,23 @@ function drawDiagram(diag, xp, yp, color, panel, dn, dText, dText2 = ""){
             drawTextRotEmp(union(dA,dI[2]).join(''),xp,yp+55,color,"middle",0,panel);
             drawTextRotEmp(union(dA,dI[3]).join(''),xp,yp-55,color,"middle",0,panel);
             break;
+        case 5:
+            drawPentagon(xp,yp,color,panel);
+            var yp2 = yp+7.39;
+            drawText(dS,xp,yp2,color,"middle",panel);
+            var r = 48;
+            var c1 = 0.25*r*(Math.sqrt(5)-1);
+            var c2 = 0.25*r*(Math.sqrt(5)+1);
+            var s1 = 0.25*r*Math.sqrt(10+2*Math.sqrt(5));
+            var s2 = 0.25*r*Math.sqrt(10-2*Math.sqrt(5));
+            var yp2 = yp+7.39;
+            drawPentagonOut(dO,xp,yp,color,panel);
+            drawTextRotEmp(union(dA,dI[0]).join(''),xp+s2,yp2-c2,color,"middle",35,panel);
+            drawTextRotEmp(union(dA,dI[1]).join(''),xp+s1,yp2+c1,color,"middle",-70,panel);
+            drawTextRotEmp(union(dA,dI[2]).join(''),xp,yp2+r,color,"middle",0,panel);
+            drawTextRotEmp(union(dA,dI[3]).join(''),xp-s1,yp2+c1,color,"middle",70,panel);
+            drawTextRotEmp(union(dA,dI[4]).join(''),xp-s2,yp2-c2,color,"middle",-35,panel);
+            break;
         default:
             drawText(`Subj: ${dS}`,xp-60,yp-46,"red","start",panel);
             drawText(`All: ${showStrSet(dA)}`,xp-60,yp-21,"red","start",panel);
@@ -94,6 +111,25 @@ function drawSquare(xp,yp,color,panel){
     figure.setAttributeNS(null, 'opacity', 1.0);
     panel.appendChild(figure);
 };
+
+function drawPentagon(xp,yp,color,panel){
+    var r = 77.39;
+    var c1 = 0.25*r*(Math.sqrt(5)-1);
+    var c2 = 0.25*r*(Math.sqrt(5)+1);
+    var s1 = 0.25*r*Math.sqrt(10+2*Math.sqrt(5));
+    var s2 = 0.25*r*Math.sqrt(10-2*Math.sqrt(5));
+    var yp2 = yp+7.39;
+
+    var thePoints = `${xp}, ${yp2-r} ${xp+s1}, ${yp2-c1} ${xp+s2}, ${yp2+c2} ${xp-s2}, ${yp2+c2} ${xp-s1}, ${yp2-c1}`;
+    var figure = document.createElementNS('http://www.w3.org/2000/svg', 'polygon');
+    figure.setAttributeNS(null, 'stroke', color);
+    figure.setAttributeNS(null, 'stroke-width', 3);
+    figure.setAttributeNS(null, 'points', thePoints);
+    figure.setAttributeNS(null, 'fill', "url(#gradient)");
+    figure.setAttributeNS(null, 'opacity', 1.0);
+    panel.appendChild(figure);
+};
+
 
 
 function drawEllipseBars(xp,yp,color,panel){
@@ -170,3 +206,46 @@ function drawSquareOut(dO,xp,yp,color,panel){
         drawText(`${dO[i]}?`,xp+120,yp-60+1.4*(i*22),color,"end",panel);
     };
 };
+
+
+function drawPentagonOut(dO,xp,yp,color,panel){
+
+    var r = 107.39;
+    var c1 = 0.25*r*(Math.sqrt(5)-1);
+    var c2 = 0.25*r*(Math.sqrt(5)+1);
+    var s1 = 0.25*r*Math.sqrt(10+2*Math.sqrt(5));
+    var s2 = 0.25*r*Math.sqrt(10-2*Math.sqrt(5));
+    var yp2 = yp+10.39;
+
+    var p1 = [xp, yp2-r];
+    var p2 = [xp+s1, yp2-c1];
+    var p3 = [xp+s2, yp2+c2];
+    var p4 = [xp-s2, yp2+c2];
+    var p5 = [xp-s1, yp2-c1];
+
+    var pos = [];
+    pos[0] = getIntermediatePoint(p1,p2,0.33);
+    pos[1] = getIntermediatePoint(p2,p1,0.33);
+    pos[2] = getIntermediatePoint(p1,p5,0.33);
+    pos[3] = getIntermediatePoint(p5,p1,0.33);
+    pos[4] = getIntermediatePoint(p2,p3,0.33);
+    pos[5] = getIntermediatePoint(p3,p2,0.33);
+    pos[6] = getIntermediatePoint(p5,p4,0.33);
+    pos[7] = getIntermediatePoint(p4,p5,0.33);
+    
+    for(var i=0; i<dO.length && i<8; i++){
+        drawText(`${dO[i]}?`,pos[i][0],pos[i][1],color,"middle",panel);
+    };
+
+    for(var i=8; i<dO.length; i++){
+        drawText(`${dO[i]}?`,xp+130,yp-60+1.4*((i-8)*22),color,"end",panel);
+    };
+
+
+};
+
+function getIntermediatePoint(p1,p2,portion){
+    px = p1[0]+portion*(p2[0]-p1[0]);
+    py = p1[1]+portion*(p2[1]-p1[1]);
+    return [px,py];
+}
